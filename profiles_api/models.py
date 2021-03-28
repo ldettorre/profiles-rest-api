b8d501b21f516241b4a28133539fca6b9f471987
Luca D'Ettorre <ldettorre21@gmail.com>
2021-03-28 00:52:54 +0000

    Fixed a bug in the UserProfileManager class where I had create_super_user instead of create_superuser. This threw an error after attempting to create a superuser

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -11,14 +11,14 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('Users must have an email address')
         
         email = self.normalize_email(email)
-        user = self.model(email=emailm name=name)
+        user = self.model(email=email, name=name)
     
         user.set_password(password)
         user.save(using=self._db)
 
         return user 
     
-    def create_super_user(self, email, name, password):
+    def create_superuser(self, email, name, password):
         '''Create and save a new superuser'''
         user = self.create_user(email, name, password)
         user.is_superuser = True
